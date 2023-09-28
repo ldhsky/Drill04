4f1d68b950621a4c878341cd695eabe4ddd547fd
ldhsky <ldhsky1027@gmail.com>
2023-09-28 18:20:48 +0900

    idle, left 결과 확인 후 남은 방향만 추가. 이미지 수정 필요

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -40,13 +40,85 @@ moving = True
 
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 xdir, ydir = 0, 0
+xidleframe, xidle = 142, 0
+yidleframe, yidle = 267, 0
 
 while moving:
     clear_canvas()
     tur_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(30, 56, 50, 50, x, y, 150, 150)
+
+    x += xdir * 5
+    y += ydir * 5
+
+    # Idle 상태
+    if 0 == xdir and 0 == ydir:
+        if 0 <= xidle % 15 and 4 >= xidle % 15:
+            character.clip_draw(142, 267, 40, 42, x, y, 150, 150)
+
+        elif 5 <= xidle % 15 and 9 >= xidle % 15:
+            character.clip_draw(183, 267, 65, 37, x, y, 250, 150)
+
+        elif 10 <= xidle % 15 and 14 >= xidle % 15:
+            character.clip_draw(249, 267, 39, 43, x, y, 150, 150)
+
+        xidle += 1
+
+    # Left 상태
+    elif -1 == xdir and 0 == ydir:
+        if 0 <= xidle % 15 and 4 >= xidle % 15:
+            character.clip_draw(123, 157, 59, 52, x, y, 250, 150)
+
+        elif 5 <= xidle % 15 and 9 >= xidle % 15:
+            character.clip_draw(183, 157, 60, 39, x, y, 250, 150)
+
+        elif 10 <= xidle % 15 and 14 >= xidle % 15:
+            character.clip_draw(243, 157, 60, 40, x, y, 250, 150)
+
+        xidle += 1
+
+    # Right 상태
+    elif 1 == xdir and 0 == ydir:
+        if 0 <= xidle % 15 and 4 >= xidle % 15:
+            character.clip_draw(123, 157, 59, 52, x, y, 250, 150)
+
+        elif 5 <= xidle % 15 and 9 >= xidle % 15:
+            character.clip_draw(183, 157, 60, 39, x, y, 250, 150)
+
+        elif 10 <= xidle % 15 and 14 >= xidle % 15:
+            character.clip_draw(243, 157, 60, 40, x, y, 250, 150)
+
+        xidle += 1
+
+    # Up 상태
+    elif 0 == xdir and 1 == ydir:
+        if 0 <= xidle % 15 and 4 >= xidle % 15:
+            character.clip_draw(123, 157, 59, 52, x, y, 250, 150)
+
+        elif 5 <= xidle % 15 and 9 >= xidle % 15:
+            character.clip_draw(183, 157, 60, 39, x, y, 250, 150)
+
+        elif 10 <= xidle % 15 and 14 >= xidle % 15:
+            character.clip_draw(243, 157, 60, 40, x, y, 250, 150)
+
+        xidle += 1
+
+    # Down 상태
+    elif 0 == xdir and -1 == ydir:
+        if 0 <= xidle % 15 and 4 >= xidle % 15:
+            character.clip_draw(123, 157, 59, 52, x, y, 250, 150)
+
+        elif 5 <= xidle % 15 and 9 >= xidle % 15:
+            character.clip_draw(183, 157, 60, 39, x, y, 250, 150)
+
+        elif 10 <= xidle % 15 and 14 >= xidle % 15:
+            character.clip_draw(243, 157, 60, 40, x, y, 250, 150)
+
+        xidle += 1
+
+
+
 
     update_canvas()
     handle_events()
 
-    delay(0.05)
+    delay(0.02)
